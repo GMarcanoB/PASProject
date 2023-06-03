a29c52fdab2cd8435c75dd58a0f52496423b4f2c
GeorgelysM <geogemar@gmail.com>
2023-06-03 21:53:29 +0200

    Create splash screen

--- app/src/main/java/com/projecstsft/pasproject/MainActivity.kt
+++ app/src/main/java/com/projecstsft/pasproject/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.projecstsft.pasproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        /*Thread.sleep(1000)
+        screenSplash.setKeepOnScreenCondition{true}*/
+        /*val intent = Intent(this, SplashActivity::class.java)
+        startActivity(intent)
+        finish()*/
+
+
     }
 }
