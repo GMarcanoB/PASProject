5bda6b3f6a8d9a8d86d6a9ecfdd9540b36e3783c
GeorgelysM <geogemar@gmail.com>
2023-06-09 01:29:19 +0200

    Update code register user inside Register Activity

--- app/src/main/java/com/projecstsft/pasproject/RegisterActivity.kt
+++ app/src/main/java/com/projecstsft/pasproject/RegisterActivity.kt
@@ -2,41 +2,36 @@ package com.projecstsft.pasproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayoutStates.TAG
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        auth = Firebase.auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
         val email : TextView = findViewById(R.id.RegEmail)
         val password : TextView = findViewById(R.id.regPassword)
-        val newUser : Button = findViewById(R.id.btnRegister)
-       /* val dirlogin : TextView = findViewById(R.id.loginIn)
-        dirlogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }*/
-        newUser.setOnClickListener {
+        val btnregister : Button = findViewById(R.id.btnRegister)
+
+        btnregister.setOnClickListener {
             when {
                 password.text.toString().isEmpty() || email.text.toString().isEmpty() -> {
                     Toast.makeText(this, "Email o contraseña incorrectos", Toast.LENGTH_SHORT).show()
                 }
                 else -> {
-                    newRegister(email.text.toString(), password.text.toString())
+                    registerUser(email.text.toString(), password.text.toString())
                 }
             }
 
@@ -44,32 +39,31 @@ class RegisterActivity : AppCompatActivity() {
 
     }
 
-    private fun updateUI(user: FirebaseUser?) {
-
-    }
-
     fun onLinkClick(view: View) {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
-     private fun newRegister(mail:String, pwd:String){
-         auth.createUserWithEmailAndPassword(mail, pwd)
-             .addOnCompleteListener(this) { task ->
-                 if (task.isSuccessful) {
-                     // Sign in success, update UI with the signed-in user's information
-                     Log.d(TAG, "createUserWithEmail:success")
-                     val user = auth.currentUser
-                     updateUI(user)
-                 } else {
-                     // If sign in fails, display a message to the user.
-                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                     Toast.makeText(
-                         baseContext,
-                         "Authentication failed.",
-                         Toast.LENGTH_SHORT,
-                     ).show()
-                     updateUI(null)
-                 }
-             }
-     }
+
+    private fun registerUser(email: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Registro exitoso, el usuario ha sido creado
+                    val user: FirebaseUser? = firebaseAuth.currentUser
+                    // Realiza las acciones necesarias después del registro exitoso
+                } else {
+                    // Ocurrió un error durante el registro
+                    try {
+                        throw task.exception!!
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        // La dirección de correo electrónico no es válida
+                    } catch (e: FirebaseAuthUserCollisionException) {
+                        // La dirección de correo electrónico ya está en uso por otro usuario
+                    } catch (e: Exception) {
+                        // Otro tipo de error
+                    }
+                }
+            }
+    }
+
 }
