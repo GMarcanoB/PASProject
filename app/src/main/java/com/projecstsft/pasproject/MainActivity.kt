b8623a86a087b711c00ee43cab0c0d6327f4b8e6
Erich <erichfgb@gmail.com>
2023-06-14 18:00:04 +0200

    feat: Binding setting activity with main activity

--- app/src/main/java/com/projecstsft/pasproject/MainActivity.kt
+++ app/src/main/java/com/projecstsft/pasproject/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.projecstsft.pasproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.projecstsft.pasproject.databinding.ActivityMainBinding
+import setting.SettingActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        mainBinding.BtnSetting.setOnClickListener { navToSetting() }
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    private fun navToSetting() {
+        startActivity(Intent(this, SettingActivity::class.java))
     }
 }
