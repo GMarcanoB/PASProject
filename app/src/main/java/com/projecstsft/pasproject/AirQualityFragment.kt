1e5f4cc36a97160fe0223c50240416129ff1411b
Erich <erichfgb@gmail.com>
2023-06-16 17:45:01 +0200

    feat: Create the basic functionality to fetch pollution from IQAir

--- app/src/main/java/com/projecstsft/pasproject/AirQualityFragment.kt
+++ app/src/main/java/com/projecstsft/pasproject/AirQualityFragment.kt
@@ -5,20 +5,79 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.*
+import org.json.JSONObject
+import android.util.Log
+import android.widget.TextView
+import java.io.IOException
 
 class AirQualityFragment : Fragment() {
+    private val apiKey = "d1542388-a4ef-4ed0-9021-ce78bc21a477" // API key de IQAir
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_air_quality, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fetchAirQualityData()
+
+    }
+
+    private fun fetchAirQualityData() {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://api.airvisual.com/v2/nearest_city?key=$apiKey")
+            .build()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    // Manejar error de conexión
+                    Log.e("AirQualityFragment", "Error de conexión: ${e.message}")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val jsonData = response.body?.string()
+                    val airQualityIndex = parseAirQualityIndex(jsonData)
+                    updateEmoji(airQualityIndex)
+                }
+            })
+        }
+    }
+
+    private fun parseAirQualityIndex(jsonData: String?): Int {
+        return JSONObject(jsonData)
+            .getJSONObject("data")
+            .getJSONObject("current")
+            .getJSONObject("pollution")
+            .getInt("aqius")
+    }
+
+    private fun updateEmoji(airQualityIndex: Int) {
+        val emoji: String = when {
+            airQualityIndex <= 50 -> "😀" // Buena calidad del aire
+            airQualityIndex <= 100 -> "😊" // Calidad del aire moderada
+            airQualityIndex <= 150 -> "😷" // Calidad del aire no saludable para grupos sensibles
+            else -> "🤢" // Calidad del aire no saludable
+        }
+
+        Log.d("emoji", emoji)
+
+//        activity?.runOnUiThread {
+//            emojiTextView.text = emoji
+//        }
+    }
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
